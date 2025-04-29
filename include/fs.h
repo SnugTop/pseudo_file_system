5902eabccb44d34840969f4cc4e141df07ad58e7
Anthony Duan <123067489+ADuan2003@users.noreply.github.com>
2025-04-29 06:10:42 -0400

    Update fs.h

--- include/fs.h
+++ include/fs.h
@@ -5,12 +5,13 @@
 //since we have 1024 data blocks and 1024 files, we assume max file size is 1024 bytes i.e. data block size
 #define MAX_DATA_BYTES = 1017
 
+//I have no clue if this is your plan for the file structure, but as opposed to a directory...
+
 typedef struct {
     char d_name[MAX_NAME_LENGTH];  // File or directory name -- size 12 bytes
-    unsigned char d_type = 0;            // 0 = unfilled, 1 = directory, 2 = file -- size 1 byte
     short d_off = 0;                   // Location of pointer, initially 0 and max 1023 -- size 2 bytes
     short data_size = 0;          // How many bytes of data -- size 2 bytes
-    char data[MAX_DATA_BYTES];    // Actual file contents -- size 1017 bytes
+    char data[MAX_DATA_BYTES];    // Actual file contents -- size 1018 bytes
 } FILE_BLOCK;
 
 #endif
