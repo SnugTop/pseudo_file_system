ddccfd0ba37ec34bb27c09707ab190540df85a23
Anthony Duan <123067489+ADuan2003@users.noreply.github.com>
2025-05-01 12:30:56 -0400

    Update fs.h

--- include/fs.h
+++ include/fs.h
@@ -4,15 +4,17 @@
 #include dir.h //for MAX_NAME_LENGTH
 //since we have max 15 data blocks and 1024 bytes per data block
 #define MAX_DATA_BYTES = 1017
+#include inode.h
+#include disk.h
+#include dir.h
 
 //I have no clue if this is your plan for the file structure, but as opposed to a directory...
 
 typedef struct {
-    short inode;                    //corresponding inode
-    short d_off = 0;                   // Location of pointer, initially 0
-    short data_size = 0;          // How many bytes of data -- size 2 bytes
-    unsigned char d_type = 2;
-    char* data;    // Actual file contents -- size 1018 bytes
+    short inode;                    //corresponding inode -- 16*block + loc, loc being inode index in block
+    short data_block_used = 0;      //which data block is being used
+    short loc_data_in_block = 0;    //where I am within that data block
+    char* buffer;                   //unspecified size
 } PDOS_FILE;
 
 #endif
