065d6cbc690ff2645bf388b4cecaacbd950ad5c2
Anthony Duan <123067489+ADuan2003@users.noreply.github.com>
2025-05-01 19:28:37 -0400

    Update inode.h

--- include/inode.h
+++ include/inode.h
@@ -7,11 +7,10 @@
 #define INODE_ENTRIES_PER_INODE_BLOCK 16
 
 typedef struct {
-    unsigned int size; //4 bytes
+    unsigned short size; //2 bytes
     time_t file_mod_time; //8 bytes
-    unsigned short data_blocks_used; //2 bytes
     unsigned short data_blocks[MAX_DATA_BLOCKS]; //30 bytes
-    unsigned char fill[20]; // padding //20 bytes
+    unsigned char fill[20]; // padding //24 bytes
 } INODE_ENTRY; //64 bytes
 
 typedef struct {
