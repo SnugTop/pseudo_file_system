c2cadf25a220a89e52450279c79211fe9219449a
Anthony Duan <123067489+ADuan2003@users.noreply.github.com>
2025-05-04 02:26:00 -0400

    Update test_mkdisk.c

--- test/test_mkdisk.c
+++ test/test_mkdisk.c
@@ -16,5 +16,19 @@ int main() {
         return 1;
     }
 
+    //parts c through h of the assignment code to write:
+    write1_funcC(); //c
+    read1_funcD(); //d 
+    write2_funcC(); //e--repeat c
+    read2_funcD(); //e--repeat d
+    write1_funcF(); //f--write
+    read1_funcF(); //f--read 1
+    read2_funcD(); //f--read 2
+    write2_funcF(); //g--write
+    read1_funcF(); //g--read 1
+    read2_funcF(); //g--read 2
+    print_files("file1.txt"); //h--file 1
+    print_files("file2.txt"); //h--file 2
+    
     return 0;
 }
