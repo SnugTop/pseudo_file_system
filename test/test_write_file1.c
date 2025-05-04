537f7261c46638d071a75853a07e96c76ab297b3
Anthony Duan <123067489+ADuan2003@users.noreply.github.com>
2025-05-04 02:10:37 -0400

    Update test_write_file1.c

--- test/test_write_file1.c
+++ test/test_write_file1.c
@@ -7,11 +7,36 @@
 //use file-system from (b)
 
 //part (c) for file1.txt
-PDOS_FILE * pf = pdos_open("file1.txt", "w"); //creates file
+int write1_funcC() {
+  PDOS_FILE * pf1 = pdos_open("file1.txt", "w"); //creates file
 
-for (int count = 0; count < 1023; count++) {
-  int c = 'A' + (count % 26); //A-Z, A-Z, etc.
-  //a char but input as an int
-  pdos_fputc(c, pf);
+  for (int count = 0; count < 1023; count++) { //fills in each in turn
+    int c = 'A' + (count % 26); //A-Z, A-Z, etc.
+    //a char but input as an int
+    pdos_fputc(c, pf1);
+  }
+  pdos_fclose(pf1);
+  return 0;
 }
-pdos_fclose(pf);
+
+//part (f) for writing to file1
+int write1_funcC() {
+  PDOS_FILE * pf2 = pdos_open("file1.txt", "w"); //creates file
+
+  //skips through already-written bytes
+  for (int count = 0; count < 1023; count++) { //fills in each in turn
+    //a char but input as an int
+    pdos_getc(c, pf2);
+  }
+
+  //newly-written bytes
+  for (int count = 0; count < 1024; count++) { //fills in each in turn
+    int c = 'A' + (count % 26); //A-Z, A-Z, etc.
+    //a char but input as an int
+    pdos_fputc(c, pf2);
+  }
+
+  //close file
+  pdos_fclose(pf2);
+  return 0;
+}
