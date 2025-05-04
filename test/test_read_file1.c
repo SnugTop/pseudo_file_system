c941e7f50762311ec58d293b6a3028514c4b7fa7
Anthony Duan <123067489+ADuan2003@users.noreply.github.com>
2025-05-04 00:10:05 -0400

    Update test_read_file1.c

--- test/test_read_file1.c
+++ test/test_read_file1.c
@@ -4,18 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h> 
 
-//use file-system from (b)
-
-//part (c) for file1.txt
-PDOS_FILE * pf = pdos_open("file1.txt", "w"); //creates file
-
-for (int count = 0; count < 1023; count++) {
-  int c = 'A' + (count % 26); //A-Z, A-Z, etc.
-  //a char but input as an int
-  pdos_fputc(c, pf);
-}
-pdos_fclose(pf);
-
 //part (d) for file1.txt
 
 PDOS_FILE * pf1 = pdos_open("file1.txt", "r");
