ac257d133e2414deb1423db174491142ef477d0c
Anthony Duan <123067489+ADuan2003@users.noreply.github.com>
2025-05-04 02:11:06 -0400

    Update test_write_file2.c

---
+++ test/test_write_file2.c
@@ -0,0 +1,42 @@
+#include "disk.h"
+#include "inode.h"
+#include "dir.h"    
+#include <stdio.h>
+#include <stdlib.h> 
+
+//use file-system from (b)
+
+//copy part (c) in part (e) for file2.txt
+int write1_funcC() {
+  PDOS_FILE * pf1 = pdos_open("file2.txt", "w"); //creates file
+
+  for (int count = 0; count < 1023; count++) { //fills in each in turn
+    int c = 'A' + (count % 26); //A-Z, A-Z, etc.
+    //a char but input as an int
+    pdos_fputc(c, pf1);
+  }
+  pdos_fclose(pf1);
+  return 0;
+}
+
+//part (f) for writing to file2
+int write1_funcC() {
+  PDOS_FILE * pf2 = pdos_open("file2.txt", "w"); //creates file
+
+  //skips through already-written bytes
+  for (int count = 0; count < 1023; count++) { //fills in each in turn
+    //a char but input as an int
+    pdos_getc(c, pf2);
+  }
+
+  //newly-written bytes
+  for (int count = 0; count < 1024; count++) { //fills in each in turn
+    int c = 'A' + (count % 26); //A-Z, A-Z, etc.
+    //a char but input as an int
+    pdos_fputc(c, pf2);
+  }
+
+  //close file
+  pdos_fclose(pf2);
+  return 0;
+}
