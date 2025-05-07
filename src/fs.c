4b81cbeafb69e5f7d252c11fa5e2a616723b7eb7
Cameron Daly <camjdaly@gmail.com>
2025-05-06 21:41:08 -0700

    addend anthony's updated fclose function

--- src/fs.c
+++ src/fs.c
@@ -158,6 +158,29 @@ void pdos_fputc(int b, PDOS_FILE *pf) {
 
 void pdos_fclose(PDOS_FILE *pf) {
     if (pf) {
+        if (pf->modeW == 0) { //cannot write buffer
+            free(pf);
+            return;
+        }
+
+        //write out buffer
+
+        //find Inode Block
+        unsigned short ib = pf->inode_block;
+        unsigned short ie = pf->inode_index; 
+        INODE_BLOCK i_block;
+        block_read(ib, &i_block);
+        INODE_ENTRY i_entry = i_block.inodes[ie];
+
+        i_block.inodes[ie].size = sizeof(pf->buffer) / sizeof(char);
+        i_block.inodes[ie].data_blocks_used = 1; //buffer cannot take up more than one block
+
+        DATA_BLOCK d_b;
+        block_read(i_entry.data_blocks[0], &d_b); //other blocks may have been allocated; they don't really matter
+        for (int count = 0; count < i_block.inodes[ie].size; count++) {
+            d_b.data[count] = pf->buffer[count]; //replace whatever byte is in there
+        } 
+        
         free(pf); // not deallocating memory blocks, just removing the pointer
     }
 }
